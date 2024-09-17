93822f755c9d35e45f8d7b3bb6f31c58d600a3c9
joschonarth <joschonarth@gmail.com>
2024-09-17 00:00:56 -0300

    Update Spotify Clone

--- src/app/components/PlaylistPreview.tsx
+++ src/app/components/PlaylistPreview.tsx
@@ -10,7 +10,7 @@ export function PlaylistPreview({ imageSrc, title }: PlaylistPreviewProps) {
         <a href="" className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
             <Image src={imageSrc} width={104} height={104} alt={title}/>
             <strong>{title}</strong>
-            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
                 <i className="fa-solid fa-play text-xl"></i>
             </button>
         </a>
